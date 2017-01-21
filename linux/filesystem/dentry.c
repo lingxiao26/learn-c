5c7bde5eb71792bf042f6b22d49281bb30e6eb9a
lilingxiao <445864543@qq.com>
2017-01-22 00:25:24 +0800

    简版ls的实现

---
+++ linux/filesystem/dentry.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <errno.h>
+
+
+int main(int argc, char *argv[])
+{
+    if ( argc != 2 ) {
+        fprintf(stderr, "Usage: %s DIR\n", __FILE__);
+        exit(1);
+    }
+    char *mdzz = argv[1];
+    struct dirent *ds = NULL;
+
+    DIR *dp = opendir(mdzz);
+
+    if ( dp == NULL ) {
+        perror("opendir failed: ");
+        exit(1);
+    }
+    while ( (ds = readdir(dp)) != NULL ) {
+        if ( errno ) {
+            perror("Error: ");
+            exit(1);
+        }
+        printf("inode: %d\tfilename: %s\n", ds->d_ino, ds->d_name);
+    }
+
+    return 0;
+}
